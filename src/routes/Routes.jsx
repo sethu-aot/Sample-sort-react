d50f294a53b383ecd2440a79415c5c1fb6e58972
sethu-aot <sethu.satheesh@aot-technologies.com>
2024-07-30 03:41:31 -0700

    initial commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Index from '../pages/Index';
+
+
+function AppRouter() {
+  return (
+    <div>
+      <Router>
+            <Routes>
+                <Route path="/" element={<Index />} />
+            </Routes>
+        </Router>
+
+    </div>
+  )
+}
+
+export default AppRouter
